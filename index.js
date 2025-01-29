46fcd3868ba3a4fb8bdbadb42e13977833ddc3f9
Rodrigo Herrera <rodrigoherreraitl@gmail.com>
2025-01-29 03:17:17 -0600

    remove unnecesary stuff

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import express from 'express';
+import express from "express";
 
-import pkg from 'pg';
-const { Pool} = pkg
-import bodyParser from 'body-parser';
-import path from 'path';
-import { __dirname } from './dirname.js';
-import { registerUser,validateUser,setToFalse,setToTrue } from './helpers/storedProcedure.js';
-import cors from 'cors';
+import pkg from "pg";
+const { Pool } = pkg;
+import bodyParser from "body-parser";
+import path from "path";
+import { __dirname } from "./dirname.js";
+import {
+  registerUser,
+  validateUser,
+  setToFalse,
+  setToTrue,
+} from "./helpers/storedProcedure.js";
+import cors from "cors";
 
 const port = process.env.PORT || 5006;
 const app = express();
@@ -17,128 +22,99 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.post('/newUser', async (req, res) => {
-  console.log('req.body', req.body)
-  let querytest=registerUser(req.body)
-  console.log('querytest', querytest)
+app.post("/newUser", async (req, res) => {
+  let querytest = registerUser(req.body);
+
   try {
-    const client = await pool.connect();
-    const result = await client.query(querytest);
-    client.release();
-    res.status(200).send({ success: 'User Added to the database' });
+    var client = await pool.connect();
+    var result = await client.query(querytest);
+    res.status(200).send({ success: "User Added to the database" });
   } catch (err) {
-    console.error('Error during user registration:', err.message);
-    res.status(400).send({ error: (err.message) });
+    console.error("Error during user registration:", err.message);
+    res.status(400).send({ error: err.message });
+  } finally {
+    client.release();
   }
 });
 
+app.post("/checkUser", async (req, res) => {
+  let querytest = validateUser(req.body);
 
-app.post('/checkUser', async (req, res) => {
-  console.log('req.body', req.body)
-  let querytest=validateUser(req.body)
-  console.log('querytest', querytest)
   try {
-    console.log('sadasvd')
-    const client = await pool.connect();
-    const result = await client.query(querytest);
-    client.release();
-    console.log(result.rows[0])
-    if(result.rows[0]==true)
-    {
-    res.status(200).send({ response:'valid password and email' });
-    }
-    else if(result.rows[0]==false){
-      res.status(400).send({ response:'Invalid password and email' }); 
+    var client = await pool.connect();
+    var result = await client.query(querytest);
+
+    if (result.rows[0].validatepasswordandemail == true) {
+      res.status(200).send({ response: "valid password and email" });
+    } else if (result.rows[0].validatepasswordandemail == false) {
+      res.status(400).send({ response: "Invalid password and email" });
     }
   } catch (err) {
-    console.error('Error during user registration:', err.message);
-    res.status(400).send({ response: (err.message) });
+    console.error("Error during user registration:", err.message);
+    res.status(400).send({ response: err.message });
+  } finally {
+    client.release();
   }
-  
 });
 
+app.post("/setFalse", async (req, res) => {
+  let querytest = setToFalse(req.body);
 
-app.post('/setFalse', async (req, res) => {
-  console.log('req.body', req.body)
-  let querytest=setToFalse(req.body)
-  console.log('querytest', querytest)
   try {
-    const client = await pool.connect();
-    const result = await client.query(querytest);
-    client.release();
-    console.log(result.rows[0])
-    res.status(200).send({ response:'Their status changed to False' });
+    var client = await pool.connect();
+    var result = await client.query(querytest);
+
+    res.status(200).send({ response: "Their status changed to False" });
   } catch (err) {
-    console.error('Error during user registration:', err.message);
-    res.status(400).send({ response: (err.message) });
+    console.error("Error during user registration:", err.message);
+    res.status(400).send({ response: err.message });
+  } finally {
+    client.release();
   }
-  
 });
 
+app.post("/setTrue", async (req, res) => {
+  let querytest = setToTrue(req.body);
 
-app.post('/setTrue', async (req, res) => {
-  console.log('req.body', req.body)
-  let querytest=setToTrue(req.body)
-  console.log('querytest', querytest)
   try {
-    const client = await pool.connect();
-    const result = await client.query(querytest);
-    client.release();
-    console.log(result.rows[0])
-    res.status(200).send({ response:'Their status changed to True' });
+    var client = await pool.connect();
+    var result = await client.query(querytest);
+
+    res.status(200).send({ response: "Their status changed to True" });
   } catch (err) {
-    console.error('Error during user registration:', err.message);
-    res.status(400).send({ response: (err.message) });
+    console.error("Error during user registration:", err.message);
+    res.status(400).send({ response: err.message });
+  } finally {
+    client.release();
   }
-  
 });
 
-app.get('/lastLogin', async (req, res) => {
-  console.log('req.body', req.body)
-  let querytest=`SELECT CONCAT(first_name, ' ', last_name) AS full_name , occupation, email, created_at, status 
-  FROM users;`
-  console.log('querytest', querytest)
+app.get("/lastLogin", async (req, res) => {
+  let querytest = `SELECT CONCAT(first_name, ' ', last_name) AS full_name , occupation, email, created_at, status 
+  FROM users;`;
+
   try {
-    const client = await pool.connect();
-    const result = await client.query(querytest);
-    client.release();
+    var client = await pool.connect();
+    var result = await client.query(querytest);
     res.status(200).send(result.rows);
   } catch (err) {
-    console.error('Error during user registration:', err.message);
+    console.error("Error during user registration:", err.message);
     res.status(400).send();
-  }
-  
-});
-
-app.get("/db", async (req, res) => {
-  console.log(`Rendering the results of a database query for route '/db'`);
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * from users");
-    console.log(result.rows);
+  } finally {
     client.release();
-  } catch (err) {
-    console.error('Database query error:', err.message);
-    res.send("Error " + err);
   }
 });
 
-
-const server = app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+const server = app.listen(port, () => {});
 
 process.on("SIGTERM", async () => {
-  console.log("SIGTERM signal received: gracefully shutting down");
   if (server) {
-    server.close(() => {
-      console.log("HTTP server closed");
-    });
+    server.close(() => {});
   }
 });
